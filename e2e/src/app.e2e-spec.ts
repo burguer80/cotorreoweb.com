cbddd07846f3f120ca7275a9b32e22f97987a11c
Sergio Burgueño <burguer@gmail.com>
2019-07-13 15:39:01 -0700

    fix project name typo

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('Welcome to cotorreweb!');
+    expect(page.getTitleText()).toEqual('Welcome to cotorreoweb!');
   });
 
   afterEach(async () => {
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'cotorreweb'`, () => {
+  it(`should have as title 'cotorreoweb'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('cotorreweb');
+    expect(app.title).toEqual('cotorreoweb');
   });
 
   it('should render title in a h1 tag', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to cotorreweb!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to cotorreoweb!');
   });
 });
