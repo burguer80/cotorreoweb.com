4fbad9eba0e03988992ca33ee8f07432f200daef
Sergio Burgueño <burguer@gmail.com>
2019-07-13 17:14:42 -0700

    add navbar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+// Pages Components
+import { HomeComponent } from './components/pages/home/home.component';
+
+const routes: Routes = [
+  {path: '', redirectTo: '/home', pathMatch: 'full'},
+  {path: 'home', component: HomeComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
