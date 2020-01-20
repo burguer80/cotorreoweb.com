2997449527132d9fd513d9879f9c723100105277
Sergio Burgueño <burguer@gmail.com>
2020-01-19 19:17:06 -0800

    setup 2e2 testing

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('Welcome to cotorreoweb!');
+    expect(page.getTitleText()).toEqual('Puro cotorreo WEB!');
   });
 
   afterEach(async () => {
